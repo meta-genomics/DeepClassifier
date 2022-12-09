9168ed6c0c9abec926fd48fe37d8c5b3fac2d34f
Fernando Fontove Herrera <45862886+Fontove@users.noreply.github.com>
2022-12-09 13:49:32 -0600

    Add files via upload

--- extractFeatures.py
+++ extractFeatures.py
@@ -3,12 +3,13 @@ import os.path
 import gzip 
 
 NUMBER_OF_NUCLEOTIDES = 4
-NUCLEOTIDES_DICT = {'A':0,'a':0,'T':1,'t':1,'G':2,'g':2,'C':3,'c':3}
-ENCODED_NUCLEOTIDES_DICT = {'A':[1,0,0,0],'a':[1,0,0,0],'T':[0,1,0,0],'t':[0,1,0,0],
-                            'G':[0,0,1,0],'g':[0,0,1,0],'C':[0,0,0,1],'c':[0,0,0,1]}
+NUCLEOTIDES_DICT = {'A':0,'a':0,'C':1,'c':1,'G':2,'g':2,'T':3,'t':3}
+ENCODED_NUCLEOTIDES_DICT = {'A':[1,0,0,0],'a':[1,0,0,0],'C':[0,1,0,0],'c':[0,1,0,0],
+                            'G':[0,0,1,0],'g':[0,0,1,0],'T':[0,0,0,1],'t':[0,0,0,1]}
 
 kmerSize = 5
-
+# get kmers from a sequence of nucleotides codified in base 4
+# p is the sequence and k the size of the kmers
 def extractKmers(p, k=kmerSize):
     kmers = [0]*(len(p)-kmerSize)
     for i in range(len(p)-kmerSize):
@@ -33,7 +34,10 @@ def extractKmers(p, k=kmerSize):
         kmers[i] = kmer
     return kmers
 
-def FastqToDataFrame(fileList, test = False, encoding='onehot', testSize=100_000):
+# Three types of encoding are provided "onehot" is serial one hot, "vector" is one hot as a vector and "token" are nucleotides as integers
+# File list has all the files with only the R1 paired end and will look for the same file with "R2"
+# If test is True, the maximum number of reads processed is given by testSize
+def FastqFilesToFeatures(fileList, test = False, encoding='onehot', testSize=100_000):
     start = datetime.datetime.now()
     classNum = 0
     sequences = list()
@@ -43,8 +47,8 @@ def FastqToDataFrame(fileList, test = False, encoding='onehot', testSize=100_000
         currTime = datetime.datetime.now()
         timelapse = currTime-start    
         print(f"runtime:\t{timelapse}\t{fileNameR1}")
-
         classNum += 1
+    # Read the R2 paired end file
         if "_R1.fastq" in fileNameR1:
             fileNameR2 = fileNameR1.replace("_R1.fastq","_R2.fastq")
         r1 = gzip.open(fileNameR1,'rt')
@@ -60,102 +64,38 @@ def FastqToDataFrame(fileList, test = False, encoding='onehot', testSize=100_000
         # get read
             p = r1.readline().replace('\n','')
             q = r2.readline().replace('\n','')
-        # comment line
+        # skip comment line
             r1.readline()
             r2.readline()
-        # quality line
+        # skip quality line
             r1.readline()
             r2.readline()
-
             sequences.append(list(p))
             sequences.append(list(q))
             classes.append(classNum)
             classes.append(classNum)
     numClasses = classNum
     classesVector = list()
+# one hot encoding de la clase 
     for c in classes:
         vector = [0]*numClasses
         vector[c-1] = 1
         classesVector.append(vector)
 
+# encoding de la secuencia
     encodedSequences = list()
     for s in sequences:
         encodedSequence = list()
         for nt in s:
-            #encoded = [0] * NUMBER_OF_NUCLEOTIDES
+        # token A = 0, T = 1, G = 2, C =3 
             if encoding == 'token':
                 encodedSequence.append(NUCLEOTIDES_DICT[nt])
+        # vector A =[ 1,0,0,0], T = [0,1,0,0], ...                
             elif encoding == 'vector':
                 encodedSequence.append(ENCODED_NUCLEOTIDES_DICT[nt])
-        # one hot sequential
+        # one hot sequential AT = [1,0,0,0,0,1,0,0]
             else:
                 encodedSequence.extend(ENCODED_NUCLEOTIDES_DICT[nt])
         encodedSequences.append(encodedSequence)
     return (encodedSequences, classesVector, numClasses) #pd.DataFrame(list(zip( sequences, classess)), columns=[list(range(len(sequences[0]))),'class'])
-            
-
-            
-
-
-        
-
-if __name__ == "__main__":
-
-    dataPath = '/home/fonty/data/genomics/unam/maribel'
-    out = open(os.path.join(dataPath,f"kmerCounts.csv.gz"),'wt')
-    runs = open(os.path.join(dataPath,"fileList.txt"),'rt')
-
-    runNumber = 0
-
-    for runName in runs:
-        runNumber += 1
-        runName = runName.replace('\n','')
-        r1Name = runName + '_R1.fastq.gz'
-        r2Name = runName + '_R2.fastq.gz'
-
-        r1 = gzip.open(os.path.join(dataPath,r1Name),'rt')
-        r2 = gzip.open(os.path.join(dataPath,r2Name),'rt')
-
-        print(f"----------------------------------")
-        print(f"---- Run {runNumber} file {runName} ------")
-
-        start = datetime.datetime.now()
-        count = 0
-
-        for p,q in zip(r1,r2):
-            kmerCounts = [0] * 4**kmerSize
-            count += 1
-            if count % 100_000 == 0:
-                currTime = datetime.datetime.now()
-                timelapse = currTime-start
-                progress = count / 1_200_001
-                print(f"runtime:\t{timelapse}")
-    #            print(f"count:\t{count}")
-                print(f"progress:\t{progress*100}")        
-                print ( f"time to finish: \t {(timelapse / progress) - timelapse}")
-                #break
-            # skip read name
-            # get read
-            p = r1.readline()
-            # comment line
-            r1.readline()
-            # quality line
-            r1.readline()
-
-            
-            q = r2.readline()
-            r2.readline()
-            r2.readline()
-
-            
-            kmers = extractKmers(p,kmerSize)
-            for k in kmers:
-                kmerCounts[k] += 1
-            kmers = extractKmers(q,kmerSize)
-            for k in kmers:
-                kmerCounts[k] += 1
-
-            out.write(f"{runName}")
-            for k in range(len(kmerCounts)):
-                out.write(f",{kmerCounts[k]}")
-            out.write("\n")
+    
--- neuralNetwork.py
+++ neuralNetwork.py
@@ -7,26 +7,30 @@ import numpy as np
 from keras.utils import np_utils
 import random
 
-epochs = 20
+# parameters for tensor flow
+epochs = 30
 batchSize = 64
 
+# path where reads are stored
 path = '/mnt/d/data/genomics/unam/maribel'
+# list of files to be processed
 fileList = open( os.path.join(path,"fileList.txt"))
 fileNames = list()
+# pre pend the path to all the files
 for line in fileList:
     fileNames.append(os.path.join(path,line.replace('\n','')))
 
-#sequences, classes, numClasses = FastqToDataFrame(fileNames, test=True, encoding='token', testSize=1_00)
-sequences, classes, numClasses = FastqToDataFrame(fileNames, test=True, encoding='onehot', testSize=1_00)
+# load all the sequences and their classes 
+sequences, classes, numClasses = FastqFilesToFeatures(fileNames, test=True, encoding='onehot', testSize=10_000)
 lengthRead = len(sequences[0])
 
-#suffle samples
+# suffle samples 
 temp = list(zip(sequences, classes))
 random.shuffle(temp)
 sequences, classes = zip(*temp)
 sequences, classes = list(sequences), list(classes)
 
-
+# split train test data
 N = len(sequences)
 N90 = int(.9*N)
 
@@ -36,9 +40,10 @@ x_test = sequences[N90:-1]
 y_train = classes[0:N90]
 y_test = classes[N90:-1]
 
+# generate tensor flow model
 model = keras.models.Sequential()
 model.add(layers.InputLayer(input_shape=(lengthRead,1), name='Input_Layer'))
-model.add(layers.LSTM(128, return_sequences=False, activation='relu'))
+model.add(layers.LSTM(512, return_sequences=False, activation='relu'))
 #model.add(layers.SimpleRNN(128, return_sequences=False, activation='relu'))
 model.add(layers.Dense(numClasses, activation='Softmax', name='Output_Layer'))
 model.summary()
@@ -46,14 +51,15 @@ model.compile(optimizer='adam',
             loss='categorical_crossentropy',#sparse_categorical_crossentropy    categorical_crossentropy
             metrics=['accuracy'])
 model.fit(x = x_train, 
-        y = y_train,##Cuando haya otra base, se modifica ese 200
+        y = y_train,
         batch_size=batchSize,
         epochs=epochs,
         verbose=2,shuffle=True)
 
 
-#model.evaluate(x_test, y_test, batch_size=batchSize, verbose=2)
 
+# get performance metrics 
+#model.evaluate(x_test, y_test, batch_size=batchSize, verbose=2)
 test_predicted_labels_raw = model.predict(x_test)
 test_true_labels      = np.argmax(y_test,axis=1)
 test_predicted_labels = np.argmax(test_predicted_labels_raw,axis=1)
